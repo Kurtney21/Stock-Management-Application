6c4fd338e153b76948b8329cf9e2883edf6785eb
CurstinJR <cjrose13@protonmail.com>
2021-09-19 15:32:46 +0200

    add DBconnection

--- src/main/java/za/ac/cput/stock/management/dbconnection/DBConnection.java
+++ src/main/java/za/ac/cput/stock/management/dbconnection/DBConnection.java
@@ -1,16 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * DBConnection.java
+ * 
+ * @author Curstin Jade Rose (220275408)
+ * @author Kurtney Clyde Jantjies (218138105)
+ * @group: Second Year ADP 262s
  */
 
 package za.ac.cput.stock.management.dbconnection;
 
-/**
- *
- * @author Kurtney Clyde Jantjies (218138105)
- * @group: Second Year
- */
-public class DBConnection {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class DBConnection 
+{
+    /**
+     * Reads db.properties file and returns a 
+     * database connection.
+     * @return
+     * @throws SQLException 
+     */
+    public Connection getDerbyConnection() throws SQLException
+    {
+        Properties props = new Properties();
+        String filename = "src/main/resources/db.properties";
+        
+        // open db.properties file
+        try (var fis = new FileInputStream(filename))
+        {
+            // read db settings
+            props.load(fis);
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("db.properties error: " + ioe.getMessage());
+        }
+        
+        // get db settings
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+        
+        return DriverManager.getConnection(url, user, password);
+    }
 }
